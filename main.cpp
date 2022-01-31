4ae0650c725f4abed7d668c22efcd46d3a24eaae
Luca Nicotra <Lsnicotra44@gmail.com>
2022-01-31 12:23:17 -0600

    Added basic math functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+
 using namespace std;
 
 int main(){
@@ -8,4 +9,20 @@ int main(){
 	cout << "Enter a number: ";
 	cin >> x;
 	cout << "You entered: " << x << endl;
+}
+
+int add(int a, int b){
+	return a + b;
+}
+
+int subtract(int a, int b){
+	return a - b;
+}
+
+int multiply(int a, int b){
+	return a * b;
+}
+
+double divide (int a, int b){
+	return a / b;
 }
